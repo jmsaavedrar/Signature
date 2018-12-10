55831792c554dea0bbd30729f0db3d2ce2d97523
Jose M. Saavedra <jose.saavedra@orand.cl>
2018-12-10 12:24:41 -0300

    augmentData with simpler modification on grayscale image

--- augmentData.py
+++ augmentData.py
@@ -6,25 +6,29 @@ Created on Thu Oct 25 10:24:42 2018
 @author: jsaavedr
 DataAugmentation for signatures
 """
-
 import random
 import cv2
 import skimage.io as io
 import skimage.filters as filters
 import skimage.morphology as morph
 import skimage.transform as transf
+import skimage.restoration as restor
 import signature_utils as sutils
 import numpy as np
 import argparse
-#sys.path.append("/home/jsaavedr/Research/git/CAR-LAR")
+
 import os
+#add gaussian noise 
 def addGaussianNoise(image, scale = 10):
     image_noise = image.copy()    
     normal_dis = np.random.normal(loc = 0, scale = scale, size = image_noise.shape)
-    image_noise =  image_noise + normal_dis
-    return image_noise
+    image_noise =  np.float32(image_noise) + normal_dis
+    image_noise[image_noise>255] = 255
+    image_noise[image_noise<0] = 0
+    return np.uint8(image_noise)
 #%%
 
+#Add blobs to images,  this would be feasible in case of binary images
 #%%
 def addBlobs(image, n_blobs)   :
     """
@@ -45,7 +49,6 @@ def addBlobs(image, n_blobs)   :
     return g_image
 #%%    
 def augmentData(image, list_n_blobs):
-
     list_of_images = [image]            
     g_image = image.copy()
     g_image = filters.gaussian(g_image, sigma=1)
@@ -59,15 +62,35 @@ def augmentData(image, list_n_blobs):
     list_of_images.append(image_e)
     #add gaussian noise
     image_noise = addGaussianNoise(image, 10)
-    list_of_images.append(image_noise)        
-    
-    image_noise = addGaussianNoise(image, 20)
-    list_of_images.append(image_noise)     
-    
-    image_noise = addGaussianNoise(image, 30)
-    list_of_images.append(image_noise)        
+    g_image = sutils.toUINT8(filters.gaussian(image_noise, sigma=1))                       
+    list_of_images.append(g_image)
     return list_of_images
 
+def augmentData2(image):
+    """
+    this is simple modification of the image
+    1) restoration by denoising
+    2) Add gaussian noise with scales 10 and 20
+    3) Each noisy image is softened by gaussian filters sigmas = [0.5, 1, 2]
+    """
+    list_of_images = [image]            
+    g_image = image.copy()
+    #g_image = sutils.toUINT8((filters.gaussian(g_image, sigma=1))
+    g_image = sutils.toUINT8(restor.denoise_tv_bregman(g_image, 5))
+    list_of_images.append(g_image)
+    
+    image_noise = addGaussianNoise(image, 10)
+    list_g = [0.5, 1, 2]
+    for sigma in list_g :
+        g_image = sutils.toUINT8(filters.gaussian(image_noise, sigma=sigma))
+        list_of_images.append(g_image) 
+        
+    image_noise = addGaussianNoise(image, 20)
+    for sigma in list_g :
+        g_image = sutils.toUINT8(filters.gaussian(image_noise, sigma=sigma))
+        list_of_images.append(g_image) 
+        
+    return list_of_images
 #%%
 if __name__ == '__main__' :
     ##Define parse    
@@ -93,11 +116,13 @@ if __name__ == '__main__' :
         label = labels[iduser]
         image = io.imread(filename, as_gray = True)
         image = sutils.toUINT8(image)
-        image = sutils.normalizeSignatureIm(image)        
-        list_of_images = augmentData(image, [5,7,10])            
+        #image = sutils.normalizeSignatureIm(image)   #to use en case of binary image     
+        list_of_images = augmentData2(image)            
         for i, _image in enumerate(list_of_images) :
             _filename = "signature_" + str(label) + "_" + str(iduser) + "_" + str(i) + ".png"
             _filename = os.path.join(out_dir, _filename)
+#            cv2.imshow("a", _image)
+#            cv2.waitKey()
             cv2.imwrite(_filename, _image)            
             f_out.write(_filename + "\t" + "U-"+label + "\n")
     print("Data was saved at {}".format(out_file))
